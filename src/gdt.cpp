7208365edff60ed8d796a9414982e59b7b8b6c5f
Younes <m.younesbadr@gmail.com>
2023-12-21 18:03:49 +0200

    Encode flags and access bits

--- src/gdt.cpp
+++ src/gdt.cpp
@@ -4,12 +4,13 @@ extern Screen screen;
 
 GDT::GDT(){
     screen.WriteString("initializing GDT...\n");
+
     lastEntryAddress = (uint8_t *)gdtBaseAddress;
     GDTEntry nullDescriptor = ConstructGDTEntry(0x00000000, 0x00000000, 0x0000, 0x0000);
-    GDTEntry kernelCodeDescriptor = ConstructGDTEntry(0x00000000, 0xfffff, 0x9a, 0xc);
-    GDTEntry kernelDataDescriptor = ConstructGDTEntry(0x00000000, 0xfffff, 0x92, 0xc);
-    GDTEntry restCodeDescriptor = ConstructGDTEntry(0x00c00000, 0xfffff, 0xfa, 0xc);
-    GDTEntry restDataDescriptor = ConstructGDTEntry(0x00c00000, 0xfffff, 0xf2, 0xc);
+    GDTEntry kernelCodeDescriptor = ConstructGDTEntry(0x00000000, 0xfffff, SEGMENT_KERNEL_CODE, SEGMENT_FLAGS_DEFAULT);
+    GDTEntry kernelDataDescriptor = ConstructGDTEntry(0x00000000, 0xfffff, SEGMENT_KERNEL_DATA, SEGMENT_FLAGS_DEFAULT);
+    GDTEntry restCodeDescriptor = ConstructGDTEntry(0x00c00000, 0xfffff, SEGMENT_USER_CODE, SEGMENT_FLAGS_DEFAULT);
+    GDTEntry restDataDescriptor = ConstructGDTEntry(0x00c00000, 0xfffff, SEGMENT_USER_DATA, SEGMENT_FLAGS_DEFAULT);
 
     encodeEntry(&nullDescriptor);
     encodeEntry(&kernelCodeDescriptor);
