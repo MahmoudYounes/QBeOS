89e88ddd7eef942f1095fcbed9b2b6341e902c98
Younes <m.younesbadr@gmail.com>
2024-01-14 23:11:02 +0200

    [IDT] Adding initial idt structure

--- src/idt.h
+++ src/idt.h
@@ -5,17 +5,28 @@
 #include "common.h"
 
 class IDTEntry : public MemoryEncodeable {
-    public:
+    private:
         uint16_t offsetLow;
         uint16_t segment;
         uint16_t flags;
         uint16_t offsetHigh;
+
+    public:
         uint64_t EncodeEntryAt(uintptr_t addr);
+        void SetOffset(uint32_t offset);
+        void SetSegment(uint16_t segment);
+        void SetFlags(uint16_t flags);
 };
 
 class IDT {
     private:
         uintptr_t idtTableBase = 0x910000;
+
+        // Loads the IDT
+        void loadIDT();
+
+        // Sets up the IDT
+        void setupIDT();
     public:
         IDT();
 
