abdc406145d03e6f0dba7663bf581eb6bcc3b9b7
Younes <m.younesbadr@gmail.com>
2024-03-02 06:21:21 +0100

    Fix some comments

--- scripts/gen_compile_commands.sh
+++ scripts/gen_compile_commands.sh
@@ -2,6 +2,8 @@
 
 # a script to generate compile_commands.json compilation db. this is crucial to make clangd work correctly
 # TODO: this is a trivial task to do that does not require a whole python package to be installed do the fucking work.
+# it is as trivial as doing make -n then capture output then greping on gcc commands. structure them in a compile_commands.json
+# syntax.
 
 selfdir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 rootdir="$selfdir/.."
