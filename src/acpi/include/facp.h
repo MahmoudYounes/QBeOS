d4810f6ae9497d2113832591dd2649932ba9371f
Mahmoud Younes <m.younesbadr@gmail.com>
2024-09-29 01:22:16 +0200

    PR review

--- src/acpi/include/facp.h
+++ src/acpi/include/facp.h
@@ -4,6 +4,15 @@
 #include "include/common.h"
 
 struct FACP{
+    uint32_t signature;
+    uint32_t length;
+    uint8_t mjrver;
+    uint8_t checksum;
+    uint32_t oemidhi;
+    uint16_t oemidlo;
+    uint32_t oemrev;
+    uint32_t creatorid;
+    uint32_t creatorrev;
     uint32_t firmwctrl;
     uint32_t dsdtptr;
     uint8_t res;
